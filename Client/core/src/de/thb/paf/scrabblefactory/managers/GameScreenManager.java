6365258acf0f43f76fc8abc5846d47854a81c55d
Dominic Schiller <dominic.schiller@th-brandenburg.de>
2017-11-10 15:01:46 +0100

    Issue #17: Implement screen manager
    Implement screen manager responsible for handling screen transitions.

---
+++ Client/core/src/de/thb/paf/scrabblefactory/managers/GameScreenManager.java
@@ -0,0 +1,66 @@
+package de.thb.paf.scrabblefactory.managers;
+
+import java.util.HashMap;
+
+import de.thb.paf.scrabblefactory.ScrabbleFactory;
+import de.thb.paf.scrabblefactory.screens.IGameScreen;
+import de.thb.paf.scrabblefactory.screens.ScreenState;
+
+/**
+ * Manager class responsible handling the overall screen management.
+ *
+ * @author Dominic Schiller - Technische Hochschule Brandenburg
+ * @version 1.0
+ * @since 1.0
+ */
+public class GameScreenManager implements IGameManager {
+
+    /**
+     * Collection of active screens
+     */
+    private final HashMap<ScreenState, IGameScreen> screens;
+
+    /**
+     * The singleton instance of the GameScreenManager
+     */
+    private static final GameScreenManager instance;
+
+    /**
+     * static initializer: called when the class is loaded by the JVM
+     */
+    static {
+        instance = new GameScreenManager();
+    }
+
+    /**
+     * Get the global GameScreenManager instance.
+     * @return The global game instance
+     */
+    public static GameScreenManager getInstance() {
+        return instance;
+    }
+
+    /**
+     * Private singleton constructor
+     */
+    private GameScreenManager() {
+        this.screens = new HashMap<ScreenState, IGameScreen>();
+    }
+
+    /**
+     * Set next screen to be active.
+     * @param screen The screen to set as active
+     */
+    public void setScreen(IGameScreen screen) {
+        ScrabbleFactory.getInstance().setScreen(screen);
+    }
+
+    @Override
+    public void dispose() {
+        // going to dispose all referenced (active) screens
+        for(IGameScreen screen : this.screens.values()) {
+            screen.dispose();
+        }
+    }
+}
+
