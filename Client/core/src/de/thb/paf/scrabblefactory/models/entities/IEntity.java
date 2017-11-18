96f8858649766e294518bddfd9be33ce8abafb12
Dominic Schiller <dominic.schiller@th-brandenburg.de>
2017-11-18 13:55:09 +0100

    Issue #13: Refactored entity interface
    Refactored the global IEntity interface which inherits now from the IGameObject interface in order to generalize the component management, the parent relationship in particular.

--- Client/core/src/de/thb/paf/scrabblefactory/models/entities/IEntity.java
+++ Client/core/src/de/thb/paf/scrabblefactory/models/entities/IEntity.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.Vector2;
 
 import java.util.List;
 
+import de.thb.paf.scrabblefactory.models.IGameObject;
 import de.thb.paf.scrabblefactory.models.components.ComponentType;
 import de.thb.paf.scrabblefactory.models.components.IComponent;
 
@@ -14,13 +15,7 @@ import de.thb.paf.scrabblefactory.models.components.IComponent;
  * @version 1.0
  * @since 1.0
  */
-public interface IEntity {
-
-    /**
-     * Updates the entity based on the game's current render tick. 
-     * @param deltaTime The time passed between the last and the current frame in seconds
-     */
-     void update(float deltaTime);
+public interface IEntity extends IGameObject{
 
     /**
      * Get the entity's unique identifier.
@@ -61,26 +56,6 @@ public interface IEntity {
      boolean isTerminated();
 
     /**
-     * Get all with the entity associated components.
-     * @return List of associated components
-     */
-    List<IComponent> getAllComponents();
-
-    /**
-     * Get all with the entity associated components of certain type.
-     * @param type The components' type
-     * @return All found components of given type
-     */
-    List<IComponent> getAllComponents(ComponentType type);
-
-    /**
-     * Get specific with the entity associated component.
-     * @param id The component's unique identifier
-     * @return The found component
-     */
-    public IComponent getComponent(int id);
-
-    /**
      * Set the entity's position.
      * @param position The new position vector
      */
@@ -104,35 +79,4 @@ public interface IEntity {
      */
      void setTerminated(boolean isTerminated);
 
-    /**
-     * Add a new component to the list of associated components.
-     * @param component The component to add
-     */
-    void addComponent(IComponent component);
-
-    /**
-     * Add a list of new  components to the list of associated components.
-     * @param components List of components to add
-     */
-    void addComponents(List<IComponent> components);
-
-    /**
-     * Removes all with the entity associated components.
-     * @return The success status
-     */
-    boolean removeAllComponents();
-
-    /**
-     * Remove all components of a specific type from the entity's list of associated components.
-     * @param type The components' type
-     * @return The success status
-     */
-    boolean removeAllComponents(ComponentType type);
-
-    /**
-     * Remove a specific component component from the entity's list of associated components.
-     * @param id The entity's unique identifier
-     * @return The success status
-     */
-    boolean removeComponent(int id);
 }
