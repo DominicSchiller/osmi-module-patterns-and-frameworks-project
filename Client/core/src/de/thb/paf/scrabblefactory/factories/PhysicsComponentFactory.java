e6372d9b155ed7d103dcea61fc9c8e126e5d7a0d
Dominic Schiller <dominic.schiller@th-brandenburg.de>
2017-11-23 16:58:33 +0100

    Issue #33: Extend Physics Component Factory
    Extend the global physics component factory by supporting the creation of a rigid body physics component.

--- Client/core/src/de/thb/paf/scrabblefactory/factories/PhysicsComponentFactory.java
+++ Client/core/src/de/thb/paf/scrabblefactory/factories/PhysicsComponentFactory.java
@@ -1,5 +1,9 @@
 package de.thb.paf.scrabblefactory.factories;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+import com.codeandweb.physicseditor.PhysicsShapeCache;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
@@ -7,14 +11,19 @@ import com.google.gson.JsonObject;
 import java.lang.reflect.InvocationTargetException;
 
 import de.thb.paf.scrabblefactory.io.AssetLoader;
+import de.thb.paf.scrabblefactory.managers.WorldPhysicsManager;
 import de.thb.paf.scrabblefactory.models.IGameObject;
 import de.thb.paf.scrabblefactory.models.components.IComponent;
+import de.thb.paf.scrabblefactory.models.components.physics.RigidBodyPhysicsComponent;
 import de.thb.paf.scrabblefactory.models.components.physics.WorldPhysicsComponent;
+import de.thb.paf.scrabblefactory.settings.Settings;
 import de.thb.paf.scrabblefactory.utils.CloneComponentHelper;
 
 import static de.thb.paf.scrabblefactory.settings.Constants.Java.COMPONENTS_PACKAGE;
 import static de.thb.paf.scrabblefactory.settings.Constants.Json.JSON_KEY_NAME;
 import static de.thb.paf.scrabblefactory.settings.Constants.Json.JSON_KEY_TYPE;
+import static de.thb.paf.scrabblefactory.settings.Settings.Game.RESOLUTION;
+import static de.thb.paf.scrabblefactory.settings.Settings.Game.VIRTUAL_SCALE;
 
 /**
  * Factory class dedicated to create and assemble new physics component instances.
@@ -43,7 +52,7 @@ public class PhysicsComponentFactory {
      * @param assetLoader The asset loader instance to load required files from the games asset's directory
      */
     public PhysicsComponentFactory(AssetLoader assetLoader) {
-
+        this.assetLoader = assetLoader;
     }
 
     /**
@@ -83,7 +92,9 @@ public class PhysicsComponentFactory {
      */
     private void initPhysComponent(IComponent physicsComponent, IGameObject parent) {
         if(physicsComponent instanceof WorldPhysicsComponent) {
-            this.initWorldPhysicsComponent(physicsComponent);
+            this.initWorldPhysicsComponent((WorldPhysicsComponent) physicsComponent);
+        } else if(physicsComponent instanceof RigidBodyPhysicsComponent) {
+            this.initRigidBodyPhysicsComponent((RigidBodyPhysicsComponent) physicsComponent, parent);
         }
     }
 
@@ -91,7 +102,34 @@ public class PhysicsComponentFactory {
      * Initializes the content of a world physics component.
      * @param physicsComponent The physics component to assemble the content for
      */
-    private void initWorldPhysicsComponent(IComponent physicsComponent) {
-        ((WorldPhysicsComponent) physicsComponent).reloadWorld();
+    private void initWorldPhysicsComponent(WorldPhysicsComponent physicsComponent) {
+        WorldPhysicsManager worldPhysicsManager = WorldPhysicsManager.getInstance();
+        physicsComponent.setWorld(worldPhysicsManager.getPhysicalWorld());
+        physicsComponent.setSPS(Settings.App.FPS);
+    }
+
+    /**
+     * Initializes the content of a rigid body physics component.
+     * @param physicsComponent The physics component to setup
+     * @param parent The parent game object
+     */
+    private void initRigidBodyPhysicsComponent(RigidBodyPhysicsComponent physicsComponent, IGameObject parent) {
+        Vector2 position = parent.getPosition();
+        float scale = VIRTUAL_SCALE * RESOLUTION.virtualScaleFactor;
+
+        PhysicsShapeCache shapeCache = this.assetLoader.loadPhysicsConfiguration(
+                parent.getAssetTargetType(),
+                parent.getID());
+
+        World world = WorldPhysicsManager.getInstance().getPhysicalWorld();
+        Body body = shapeCache.createBody(
+                physicsComponent.getActiveBodyName(),
+                world,
+                scale, scale);
+//        body.setUserData(name);
+        body.setTransform(position.x, position.y, 0);
+
+        physicsComponent.setPhysicsShapeShapeCache(shapeCache);
+        physicsComponent.setBody(body);
     }
 }
