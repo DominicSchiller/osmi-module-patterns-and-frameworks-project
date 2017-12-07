f95b8dc05647a35a92640561162e566f0fef750b
Dominic Schiller <dominic.schiller@th-brandenburg.de>
2017-12-07 16:23:48 +0100

    Issue #20: Update the play screen
    Update the play screen by adding a hud system reference.

--- Client/core/src/de/thb/paf/scrabblefactory/screens/PlayScreen.java
+++ Client/core/src/de/thb/paf/scrabblefactory/screens/PlayScreen.java
@@ -3,19 +3,23 @@ package de.thb.paf.scrabblefactory.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
 
 import java.util.List;
 
 import de.thb.paf.scrabblefactory.ScrabbleFactory;
 import de.thb.paf.scrabblefactory.factories.EntityFactory;
+import de.thb.paf.scrabblefactory.factories.HUDSystemFactory;
+import de.thb.paf.scrabblefactory.models.IGameObject;
 import de.thb.paf.scrabblefactory.models.components.ComponentType;
 import de.thb.paf.scrabblefactory.models.components.IComponent;
 import de.thb.paf.scrabblefactory.models.components.graphics.IGraphicsComponent;
-import de.thb.paf.scrabblefactory.models.components.physics.RigidBodyPhysicsComponent;
 import de.thb.paf.scrabblefactory.models.entities.EntityType;
 import de.thb.paf.scrabblefactory.models.entities.IEntity;
+import de.thb.paf.scrabblefactory.models.hud.HUDSystem;
+import de.thb.paf.scrabblefactory.models.hud.HUDSystemType;
+import de.thb.paf.scrabblefactory.models.hud.IHUDComponent;
 import de.thb.paf.scrabblefactory.models.level.ILevel;
-import de.thb.paf.scrabblefactory.settings.Settings;
 import de.thb.paf.scrabblefactory.factories.LevelFactory;
 import de.thb.paf.scrabblefactory.utils.debug.VisualGameDebugger;
 
@@ -35,6 +39,7 @@ public class PlayScreen extends GameScreen {
     private VisualGameDebugger debugRenderer;
     private OrthographicCamera camera;
     private ILevel level;
+    private HUDSystem hud;
     private IEntity player;
 
     /**
@@ -48,7 +53,14 @@ public class PlayScreen extends GameScreen {
                 false,
                 VIRTUAL_WIDTH,
                 VIRTUAL_HEIGHT);
-        ScrabbleFactory.getInstance().batch.setProjectionMatrix(this.camera.combined);
+
+        ScrabbleFactory.getInstance().batch.setProjectionMatrix(
+                this.camera.combined
+        );
+
+        ScrabbleFactory.getInstance().textBatch.setProjectionMatrix(
+                this.camera.combined.cpy().scl(1/PPM)
+        );
     }
 
     @Override
@@ -62,6 +74,7 @@ public class PlayScreen extends GameScreen {
     public void show() {
 
         this.level = new LevelFactory().getLevel(1);
+        this.hud = new HUDSystemFactory().getHUDSystem(HUDSystemType.SINGLE_PLAYER_HUD);
         this.player = new EntityFactory().getEntity(EntityType.PLAYER, 1);
         this.debugRenderer = new VisualGameDebugger();
 
@@ -77,27 +90,30 @@ public class PlayScreen extends GameScreen {
         Gdx.gl.glClearColor(1/255f, 8/255f, 15/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // TODO: Implement here...
-        ScrabbleFactory.getInstance().batch.begin();
+        Batch batch = ScrabbleFactory.getInstance().batch;
+        Batch textBatch = ScrabbleFactory.getInstance().textBatch;
 
-        //TODO Implement more elegant method
+        //TODO Implement more elegant method to render components
         List<IComponent> components = this.level.getAllComponents(ComponentType.GFX_COMPONENT);
         for(IComponent component : components) {
-            ((IGraphicsComponent) component).render(ScrabbleFactory.getInstance().batch);
+            ((IGraphicsComponent) component).render(batch);
         }
 
         components = this.player.getAllComponents(ComponentType.GFX_COMPONENT);
         for(IComponent component : components) {
-            ((IGraphicsComponent) component).render(ScrabbleFactory.getInstance().batch);
+            ((IGraphicsComponent) component).render(batch);
         }
 
-        ScrabbleFactory.getInstance().batch.end();
+        // render HUD components
+        List<IHUDComponent> hudComponents = this.hud.getHUDComponents();
+        for(IGameObject hudComponent: hudComponents) {
+            components = hudComponent.getAllComponents(ComponentType.GFX_COMPONENT);
+            for(IComponent component : components) {
+                ((IGraphicsComponent) component).render(batch);
+            }
+        }
 
-        this.debugRenderer.render(
-                ScrabbleFactory.getInstance().batch,
-//                this.camera.combined.cpy().scl(PPM)
-                this.camera.combined
-        );
+        this.debugRenderer.render(textBatch);
     }
 
     @Override
