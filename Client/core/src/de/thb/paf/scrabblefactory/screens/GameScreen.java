a7775be075f3962143f552f9b6284188c3c30a66
Dominic Schiller <dominic.schiller@th-brandenburg.de>
2017-12-08 20:19:58 +0100

    Issue #19: Extend abstract screen class by handling the window resize event in general

--- Client/core/src/de/thb/paf/scrabblefactory/screens/GameScreen.java
+++ Client/core/src/de/thb/paf/scrabblefactory/screens/GameScreen.java
@@ -11,13 +11,35 @@ package de.thb.paf.scrabblefactory.screens;
 
 public abstract class GameScreen implements IGameScreen {
 
+    /**
+     * The screen's state type
+     */
     private final ScreenState state;
+
+    /**
+     * State indicating whether the screen is set as active or not
+     */
+    boolean isActive;
+
+    /**
+     * State indicating whether the screen's components have been initialized or not
+     */
+    boolean isInitialized;
 
     /**
      * Default Constructor
      */
     GameScreen(ScreenState state) {
         this.state = state;
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        if(this.isActive && !this.isInitialized) {
+            this.show();
+        } else {
+            this.isActive = true;
+        }
     }
 
     @Override
