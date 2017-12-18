a2fceadc5f31a26e27e526f6010cb8f2a95b7174
Dominic Schiller <schiller.dominic@outlook.com>
2017-12-18 01:39:24 +0100

    Issue #34: Implement event factory

---
+++ Client/core/src/de/thb/paf/scrabblefactory/factories/EventFactory.java
@@ -0,0 +1,44 @@
+package de.thb.paf.scrabblefactory.factories;
+
+import de.thb.paf.scrabblefactory.managers.GameEventManager;
+import de.thb.paf.scrabblefactory.models.events.GameEventType;
+import de.thb.paf.scrabblefactory.models.events.IGameEvent;
+import de.thb.paf.scrabblefactory.models.events.MoveEvent;
+
+/**
+ * Factory class dedicated to create and assemble new game events.
+ *
+ * @author Dominic Schiller - Technische Hochschule Brandenburg
+ * @version 1.0
+ * @since 1.0
+ */
+public class EventFactory {
+
+    /**
+     * Get the game event instance defined by it's game event type.
+     * @param eventType The event type which defines the game event instance
+     * @return The assembled game event instance
+     */
+    public IGameEvent getGameEvent(GameEventType eventType) {
+        IGameEvent event = GameEventManager.getInstance().getGameEvent(eventType);
+        if(event != null) {
+            return event;
+        }
+
+        switch(eventType) {
+            case MOVE:
+                event = new MoveEvent();
+                break;
+            default:
+                event = null;
+                break;
+        }
+
+        // add event to the global game event manager
+        if(event != null) {
+            GameEventManager.getInstance().addGameEvent(event);
+        }
+
+        return event;
+    }
+}
